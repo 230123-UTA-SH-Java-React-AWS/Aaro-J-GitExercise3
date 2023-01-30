888323ae4c419ced81cb3c561c4be9e8a2b18c8a
AaroAce <aajackson2009@yahoo.com>
2023-01-30 17:03:19 -0500

    adding Salute

---
+++ Revature/Calculator.java
@@ -0,0 +1,20 @@
+package Revature;
+
+public class Calculator {
+
+    public void addNumbers(int x, int y) {
+         x = 3;
+         y = 2;
+    System.out.println(x + y);
+    }
+
+    public void multiplyNumbers(int x, int y) {
+        x = 4;
+        y = 2;
+    System.out.println(x * y);
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
